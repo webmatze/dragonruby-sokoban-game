b59212e86c3fe89c67a9df00fd0411652136d556
Mathias Karstädt <mathias.karstaedt@gmail.com>
2021-09-14 19:55:24 +0200

    Adds .ruby-version and some style changes

--- app/main.rb
+++ app/main.rb
@@ -1,3 +1,5 @@
+$gtk.reset
+
 class Game
   attr_gtk
 
@@ -8,19 +10,19 @@ class Game
     @tile_width = 32
     @player ||= Player.new x: 0, y: 0, width: @tile_width
     @level ||= [
-      [1,1,1,1,1,1],
-      [1,0,0,0,2,1],
-      [1,0,1,1,1,1],
-      [1,0,1,0,0,1],
-      [1,0,0,0,0,1],
-      [1,1,1,1,1,1]
+      [1, 1, 1, 1, 1, 1],
+      [1, 0, 0, 0, 2, 1],
+      [1, 0, 1, 1, 1, 1],
+      [1, 0, 1, 0, 0, 1],
+      [1, 0, 0, 0, 0, 1],
+      [1, 1, 1, 1, 1, 1]
     ]
   end
 
   def init
     @level_solids ||= @level.map_with_index do |row, y|
       row.map_with_index do |col, x|
-        x_pos = grid.left + (x*tile_width)
+        x_pos = grid.left + (x * tile_width)
         y_pos = grid.top - tile_width - (y * tile_width)
         case col
         when 1
@@ -42,19 +44,20 @@ class Game
     # render player
     outputs.primitives << player.primitives
     # render ui
-    outputs.labels << [10, 30, gtk.current_framerate]
+    debug_data = "fps: #{gtk.current_framerate.round}, ticks: #{state.tick_count}"
+    outputs.labels << [10, 30, debug_data]
   end
-  
+
   def input
     player.move_right(10) if inputs.right
     player.move_left(10) if inputs.left
     player.move_up(10) if inputs.up
     player.move_down(10) if inputs.down
-    if inputs.mouse.inside_rect? player
-      player.color = [255,255,0] 
-    else
-      player.color = [255,0,0]
-    end
+    player.color = if inputs.mouse.inside_rect? player
+                     [255, 255, 0]
+                   else
+                     [255, 0, 0]
+                   end
   end
 
   def calc
@@ -73,16 +76,19 @@ class Game
 end
 
 module Moveable
-  def move_right(amount=1)
+  def move_right(amount = 1)
     @x += amount
   end
-  def move_left(amount=1)
+
+  def move_left(amount = 1)
     @x -= amount
   end
-  def move_up(amount=1)
+
+  def move_up(amount = 1)
     @y += amount
   end
-  def move_down(amount=1)
+
+  def move_down(amount = 1)
     @y -= amount
   end
 end
@@ -90,7 +96,7 @@ end
 class TileBlock
   attr_accessor :x, :y, :w, :h, :path
 
-  def initialize x: 0, y: 0, width: 16, path: ''
+  def initialize(x: 0, y: 0, width: 16, path: '')
     @x = x
     @y = y
     @w = width
@@ -104,7 +110,7 @@ class Player < TileBlock
 
   def primitives
     [
-      { x: x, y: y, w: w, h: h, path: 'sprites/Blocks/blobLeft.png'}.sprite
+      { x: x, y: y, w: w, h: h, path: 'sprites/Blocks/blobLeft.png' }.sprite
     ]
   end
 end
@@ -112,7 +118,7 @@ end
 class Wall < TileBlock
   def primitives
     [
-      { x: x, y: y, w: w, h: h, path: 'sprites/Blocks/darkStoneBlock.png'}.sprite
+      { x: x, y: y, w: w, h: h, path: 'sprites/Blocks/darkStoneBlock.png' }.sprite
     ]
   end
 end
@@ -120,14 +126,14 @@ end
 class Floor < TileBlock
   def primitives
     [
-      { x: x, y: y, w: w, h: h, path: 'sprites/Blocks/darkDirtBlock.png'}.sprite
+      { x: x, y: y, w: w, h: h, path: 'sprites/Blocks/darkDirtBlock.png' }.sprite
     ]
   end
 end
 
 $game = Game.new
 
-def tick args
+def tick(args)
   $game.args = args
   $game.tick
 end
