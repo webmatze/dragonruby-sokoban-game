3fcfd4f5ed5b9532fc3979369171d295e0fd01e7
Mathias Karstädt <mathias.karstaedt@gmail.com>
2021-09-28 01:40:36 +0200

    Fixes rendering of storage tiles

--- app/entities/storage.rb
+++ app/entities/storage.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class NewStorage < Draco::Entity
-  component Tag(:foreground)
+  component Tag(:background)
   component Position, x: 0, y: 0
   component Sprite, path: 'sprites/sokuban_4.png'
 end
--- app/systems/render_background_sprites.rb
+++ app/systems/render_background_sprites.rb
@@ -4,10 +4,23 @@ class RenderBackgroundSprites < RenderSystem
   filter Tag(:background), Sprite, Position
 
   def tick(args)
-    sprites = entities.map do |entity|
+    # render background tiles
+    args.outputs.sprites << background_tiles.map do |entity|
       make_sprite(entity, x_offset(args), y_offset(args))
     end
+    # render Storage tiles
+    args.outputs.sprites << storage_tiles.map do |entity|
+      make_sprite(entity, x_offset(args), y_offset(args))
+    end
+  end
 
-    args.outputs.sprites << sprites
+  private
+
+  def storage_tiles
+    entities.select { |entity| entity.is_a?(NewStorage) }
+  end
+
+  def background_tiles
+    entities.reject { |entity| entity.is_a?(NewStorage) }
   end
 end
