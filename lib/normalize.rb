66742b686e8673c3f933c9a2aed8c978d04fcec6
Mathias Karstädt <mathias.karstaedt@gmail.com>
2021-09-30 21:04:32 +0200

    Adds a level normalizer for imported levels

---
+++ lib/normalize.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+class Normalize
+  def self.call(xsb)
+    rows = xsb.split("\n")
+    data = rows.map do |row|
+      row.split('')
+    end
+    player_pos = get_player_pos(data)
+    fill_floor(data, player_pos[0], player_pos[1])
+    fill_outer_area(data)
+    data
+  end
+
+  def self.fill_floor(data, x, y)
+    checked = []
+    stack = []
+    stack.push [x, y]
+
+    while stack.any?
+      x, y = stack.pop
+
+      cur_col = data[y][x]
+      next unless [' ', '@', '.', '$'].include? cur_col
+
+      data[y][x] = '-' if cur_col == ' '
+      next if checked.include? [x, y]
+
+      checked.push [x, y]
+
+      stack.push([x, y + 1]) unless checked.include?([x, y + 1])
+      stack.push([x, y - 1]) unless checked.include?([x, y - 1])
+      stack.push([x + 1, y]) unless checked.include?([x + 1, y])
+      stack.push([x - 1, y]) unless checked.include?([x - 1, y])
+    end
+  end
+
+  def self.fill_outer_area(data)
+    data.map.with_index do |row, y|
+      row.map.with_index do |col, x|
+        data[y][x] = '_' if col == ' '
+      end
+    end
+  end
+
+  def self.get_player_pos(data)
+    pos = []
+    data.each_with_index do |row, y|
+      row.each_with_index do |col, x|
+        pos = [x, y] if col.eql? '@'
+      end
+    end
+    pos
+  end
+end
---
+++ spec/lib/normalize_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require './lib/normalize'
+
+RSpec.describe Normalize do
+  describe '.get_player_pos' do
+    it 'finds correct coordinates of player' do
+      data = [
+        '   '.split(''),
+        ' @ '.split(''),
+        '   '.split('')
+      ]
+      expect(described_class.get_player_pos(data)).to eql([1, 1])
+    end
+  end
+
+  describe '#call' do
+    let(:xsb) do
+      [
+        '########  ',
+        '# ..#  #  ',
+        '# ..# $###',
+        '#  ##    #',
+        '## $   $ #',
+        ' # ##  ###',
+        ' #   $##  ',
+        ' ###  #   ',
+        '   #@ #   ',
+        '   ####   '
+      ].join("\n")
+    end
+    let(:expected_result) do
+      [
+        '########__'.split(''),
+        '#-..#--#__'.split(''),
+        '#-..#-$###'.split(''),
+        '#--##----#'.split(''),
+        '##-$---$-#'.split(''),
+        '_#-##--###'.split(''),
+        '_#---$##__'.split(''),
+        '_###--#___'.split(''),
+        '___#@-#___'.split(''),
+        '___####___'.split('')
+      ]
+    end
+    let(:result) { described_class.call(xsb) }
+
+    it 'returns an Array' do
+      expect(result).to be_an(Array)
+    end
+
+    it 'normalizes data' do
+      expect(result).to eql(expected_result)
+    end
+  end
+end
